74fa642eafa08dc4eb951dc7505907d21fc913d9
manudroid19 <yo@manueldeprada.com>
2019-02-28 00:03:20 +0100

    adaptar plot

--- plot.R
+++ plot.R
@@ -1,7 +1,10 @@
-datos <- read.table(file = "res-mixed.csv",header = TRUE,sep = "")
+archivo="resSinMhz1k-2.csv"
+datos <- read.table(file = archivo,header = TRUE,sep = "")
 erres <- read.table(file = "erres.txt",header = TRUE,sep = "")
-erres
-plot(datos$L, datos$resultado,col="white",xlab="Lineas cache (escala log)", ylab="Coste en ciclos por acceso",xaxt="n", log = "x")
+#erres
+datos=aggregate(.~L+D,datos, median)
+
+plot(datos$L, datos$resultado,col="white",xlab="Lineas cache (escala log)",main = archivo ,xaxt="n", ylim=c(8,31),log = "x",ylab="Coste en ciclos por acceso")
 axis(1,c(datos$L,c(512,4096)))
 colors = rainbow(6)[c(1,3:6)]
 
@@ -9,12 +12,17 @@ i=1
 for (value in unique(datos$D)){
   points(datos[datos$D==value, ]$L,datos[datos$D==value, ]$resultado,col=colors[i],pch=16)
   lines (datos[datos$D==value, ]$L,datos[datos$D==value, ]$resultado, type="l",lwd=3, col=colors[i])
+  if("R" %in% colnames(datos))
+  {
+    text(datos[datos$D==value, ]$L, datos[datos$D==value, ]$resultado+0.2, labels=datos[datos$D==value, ]$R, cex= 0.7)
+  }else{
   text(erres[erres$D==value, ]$L, datos[datos$D==value, ]$resultado+0.2, labels=erres[erres$D==value, ]$R, cex= 0.7)
+  }
   i=i+1
 }
 text(c(650,5500),c(21.8,21.8),c("S1=512","S2=4096"))
 abline(v=c(512,4096), lty=c(2,2))
-legend("bottom", legend=rev(paste("D =",unique(datos$D))),
+legend("topleft", legend=rev(paste("D =",unique(datos$D))),
        col=rev(colors), lty=1, cex=1, lwd=3, title="Colores: Valores de D",inset=.01,text.font=3)
 
 
